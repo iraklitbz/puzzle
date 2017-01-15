4af1040e4f471d6741d50a77ee3ea4dc1bc1c391
iraklitbz <iraklitbz@gmail.com>
2017-01-15 12:27:34 +0100

    first commit

---
+++ www/controllers/home.js
@@ -0,0 +1,14 @@
+/**
+ * Created by iraklitavberidze on 11/1/17.
+ */
+function homeCtrl ($rootScope, $scope) {
+    $scope.rotation = 0;
+
+    $scope.rotateBox = function (string) {
+        $scope.rotation = $scope.rotation === -180 ? 0 : -180;
+
+        $("." + string).css("transform", "rotateY(" + $scope.rotation + "deg)")
+
+        $('.front, .back').toggleClass('inback');
+    }
+}
