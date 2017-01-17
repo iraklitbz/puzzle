4161045d22a7a7abfb1d5730d96911e21d0d73b6
Santiago Sánchez <chadsfather@gmail.com>
2017-01-17 13:12:14 +0100

    Cambios en la detecciñón de border

--- www/controllers/home.js
+++ www/controllers/home.js
@@ -7,54 +7,41 @@ function homeCtrl ($rootScope, $scope) {
     $scope.rotateBox = function (string) {
         $scope.rotation = $scope.rotation === -180 ? 0 : -180;
 
-        $("." + string).css("transform", "rotateY(" + $scope.rotation + "deg)")
-
+        $('.' + string).css('transform', 'rotateY(' + $scope.rotation + 'deg)');
         $('.front, .back').toggleClass('inback');
     };
 
-    // target elements with the "draggable" class
     interact('.draggable')
-        .draggable({
-            // enable inertial throwing
+    	.draggable({
             inertia: true,
-            // keep the element within the area of it's parent
-            restrict: {
-                restriction: "parent"
-            },
-
             snap: {
                 targets: [
                     interact.createSnapGrid({x: 60, y: 60})
                 ]
             },
-
-
-            // call this function on every dragmove event
-            onmove: dragMoveListener
+            onmove: dragMoveListener,
+            onend: restrictContainer
         });
 
-    $('.square').on('click tap',function(event){
-        $(event.target).parent().css('z-index', 9999);
+    function restrictContainer (event) {
+    	var elePos = $(event.target).position(),
+    		eleX = elePos.left,
+    		eleY = elePos.top;
 
-    });
+    	console.log('X: ', eleX);
+    	console.log('Y: ', eleY);
+    }
 
     function dragMoveListener (event) {
         var target = event.target,
-            // keep the dragged position in the data-x/data-y attributes
             x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
             y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-        // translate the element
-        target.style.webkitTransform =
-            target.style.transform =
-                'translate(' + x + 'px, ' + y + 'px)';
+        target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 
-        // update the posiion attributes
         target.setAttribute('data-x', x);
         target.setAttribute('data-y', y);
     }
 
-    // this is used later in the resizing and gesture demos
     window.dragMoveListener = dragMoveListener;
 }
-
