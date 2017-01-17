e28ed36ea555590895b3a4fcefacade5f2317810
iraklitbz <iraklitbz@gmail.com>
2017-01-17 12:21:01 +0100

    drag

--- www/controllers/home.js
+++ www/controllers/home.js
@@ -12,20 +12,49 @@ function homeCtrl ($rootScope, $scope) {
         $('.front, .back').toggleClass('inback');
     };
 
-    if (jQuery().gridster) {
-    	var gridster;
-
-        gridster = $(".gridster ul").gridster({
-            widget_base_dimensions: [58.5, 58.5],
-            widget_margins: [1, 1],
-            helper: 'clone',
-            resize: {
-                enabled: false,
+    // target elements with the "draggable" class
+    interact('.draggable')
+        .draggable({
+            // enable inertial throwing
+            inertia: true,
+            // keep the element within the area of it's parent
+            restrict: {
+                restriction: "parent"
             },
-            min_cols: 4,
-        }).data('gridster');
+
+            snap: {
+                targets: [
+                    interact.createSnapGrid({x: 60, y: 60})
+                ]
+            },
 
 
-	}
+            // call this function on every dragmove event
+            onmove: dragMoveListener
+        });
+
+    $('.square').on('click tap',function(event){
+        $(event.target).parent().css('z-index', 9999);
+
+    });
+
+    function dragMoveListener (event) {
+        var target = event.target,
+            // keep the dragged position in the data-x/data-y attributes
+            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+        // translate the element
+        target.style.webkitTransform =
+            target.style.transform =
+                'translate(' + x + 'px, ' + y + 'px)';
+
+        // update the posiion attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+    }
+
+    // this is used later in the resizing and gesture demos
+    window.dragMoveListener = dragMoveListener;
 }
 
