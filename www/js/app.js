db27101ff6ec5cc40384e64f894213abc32928b5
iraklitbz <iraklitbz@gmail.com>
2017-01-25 10:09:42 +0100

    all levels added

--- www/js/app.js
+++ www/js/app.js
@@ -107,6 +107,27 @@ var app = angular.module('starter', ['ionic'])
                 url: '/puzzle12',
                 templateUrl: 'templates/puzzle12.html',
                 controller: 'puzzleCtrl'
+            })
+
+            .state('puzzle13', {
+                name: 'puzzle13',
+                url: '/puzzle13',
+                templateUrl: 'templates/puzzle13.html',
+                controller: 'puzzleCtrl'
+            })
+
+            .state('puzzle14', {
+                name: 'puzzle14',
+                url: '/puzzle14',
+                templateUrl: 'templates/puzzle14.html',
+                controller: 'puzzleCtrl'
+            })
+
+            .state('puzzle15', {
+                name: 'puzzle15',
+                url: '/puzzle15',
+                templateUrl: 'templates/puzzle15.html',
+                controller: 'puzzleCtrl'
             });
 
 
